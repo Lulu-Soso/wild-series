98c14fbb1489b09600166737919f20b4f71dc641
Lulu-Soso <lucas.sokha@gmail.com>
2021-05-18 17:23:46 +0200

    add navbar, favicon, files

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProgramController extends AbstractController
+{
+    /**
+     *  @Route("/programs/", name="program_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'wild Séries'
+        ]);
+    }
+}
